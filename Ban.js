09dab65a02f822299cad728fba563d90c194bc64
Lefyr <71280874+Lefyr@users.noreply.github.com>
2020-09-14 19:01:21 -0300

    Create Ban.js
    Comando de ban v12

---
+++ Ban.js
@@ -0,0 +1,47 @@
+const Discord = require('Discord.js')
+exports.run = async (bot, message, args, args_txt, chat) => {
+
+	let bUser = message.guild.member(message.mentions.users.first());
+
+	if (!message.member.hasPermission("BAN_MEMBER")) {
+		const permissão = new Discord.MessageEmbed()
+
+		.setTitle("Você Não tem permissão para executar esse comando")
+		.setTimestamp()
+		.setFooter(`${message.guild.name}`)
+		.setColor("#8B008B")
+
+		return message.reply(permissão)
+	}
+
+	if(!bUser) {
+		const mencione = new Discord.MessageEmbed()
+
+		.setTitle("Mencione o membro que deseja ser banido.")
+		.setTimestamp()
+		.setFooter(`${message.guild.name}`)
+		.setColor("#8B008B")
+
+		return message.reply(mencione)
+	}
+
+	let bReason = args.join(" ").slice(22);
+	if(!bReason) {
+		bReason = "Rasão do ban";
+
+	}
+
+	const Ban = new Discord.MessageEmbed()
+	.setTitle("Banido")
+	.setColor("8B008B")
+	.setdescription("Foi banido")
+    message.reply(Ban);
+
+ message.guild.members.ban(bUser).then(bReason);
+
+}
+
+   exports.help = {
+   	name: "ban"
+   	aliases: ["ban"]
+   }
